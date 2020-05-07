9abc0a5b90983fbfcff717d149d040db62de1978
z33p <raphael00fellipe@gmail.com>
2020-05-07 05:44:42 -0300

    MyApp extracted from main function

--- todos_mobile/lib/main.dart
+++ todos_mobile/lib/main.dart
@@ -1,86 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:flutter_redux/flutter_redux.dart';
-import 'package:todos_mobile/actions/todos_actions.dart';
-import 'package:todos_mobile/helpers/TodosProvider.dart';
-import 'package:todos_mobile/screens/MainScreen/MainScreen.dart';
-import 'package:todos_mobile/screens/TodoFormScreen/TodoForm/TodoForm.dart';
-import 'package:todos_mobile/screens/TodoFormScreen/TodoFormScreen.dart';
-import 'package:todos_mobile/store.dart';
 
-import 'models/Todo.dart';
+import 'App.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(MyApp());
-}
-
-class MyApp extends StatefulWidget {
-  MyApp({Key key}) : super(key: key) {
-    getTodosAction(store);
-  }
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final notifications = FlutterLocalNotificationsPlugin();
-
-  @override
-  void initState() {
-    super.initState();
-    todoForm.notifications = notifications;
-
-    final settingsAndroid = AndroidInitializationSettings('app_icon');
-    final settingsIOS = IOSInitializationSettings(
-        onDidReceiveLocalNotification: (id, title, body, payload) =>
-            onSelectNotification(payload));
-
-    notifications.initialize(
-        InitializationSettings(settingsAndroid, settingsIOS),
-        onSelectNotification: onSelectNotification);
-  }
-
-  Future onSelectNotification(String payload) async {
-    Todo todo = await TodosProvider.db.find(int.parse(payload));
-
-    await Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => TodoFormScreen(
-          title: todo.title,
-          todo: todo,
-          isReadingTodo: true,
-        ),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return StoreProvider<AppState>(
-      store: store,
-      child: MaterialApp(
-        title: "Tarefas",
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          inputDecorationTheme: InputDecorationTheme(
-            contentPadding:
-                EdgeInsets.symmetric(vertical: 14.0, horizontal: 8.0),
-            border: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.grey, width: 1.0),
-            ),
-          ),
-        ),
-        home: StoreConnector<AppState, List<Todo>>(
-          converter: (store) => store.state.todos,
-          builder: (context, todos) => MainScreen(
-            todos,
-            title: "Tarefas",
-          ),
-        ),
-      ),
-    );
-  }
+  runApp(App());
 }
